b69d2b25e4e517c9f098aa4d93bb8cf363b679f7
s2599932 <s2599932@bioinfmsc9.bio.ed.ac.uk>
2024-07-27 18:30:55 +0100

    Final dataset processed and fgsea script refined

--- 01-reproduced-old-dissertation/pca-loadings.R
+++
@@ -1,8 +0,0 @@
-# Extract the loadings for all features used in PCA
-pca_loading <- Loadings(so_decon_norm_seg, reduction = "pca")
-# Convert to dataframe
-pca_loading_df <- as.data.frame(pca_loading)
-# Add a new column for ensembl ID
-pca_loading_df$ensemblID <- rownames(pca_loading_df)
-
-write.csv(pca_loading_df, file = "./pca_load_features.csv", row.names = FALSE)
